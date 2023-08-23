446019512d05cc03de9f607f07d6f1152d8f37bd
StenlyAndika <stenly.andika@gmail.com>
2023-08-23 16:06:37 +0700

    fix a little bugs

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('berita', [ApiBeritaController::class, 'index']);
+// Route::get('berita', [ApiBeritaController::class, 'index']);
